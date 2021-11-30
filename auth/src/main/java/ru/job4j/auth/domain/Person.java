7cb9fa2638077bba2c12f3440c7c7b8af0e46e6a
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2021-11-30 15:11:28 +0500

    0. RESTFull. Описание архитектуры [#6884]

--- auth/src/main/java/ru/job4j/auth/domain/Person.java
+++ auth/src/main/java/ru/job4j/auth/domain/Person.java
@@ -5,7 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.Objects;
 
 @Entity
 public class Person {
@@ -39,5 +38,12 @@ public class Person {
         this.password = password;
     }
 
-    //get set hashCode equals.
+    public Person(int id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
+
+    public Person() {
+    }
 }
--- auth/src/test/java/ru/job4j/auth/controller/PersonControllerTest.java
+++ auth/src/test/java/ru/job4j/auth/controller/PersonControllerTest.java
@@ -1,58 +1,50 @@
 package ru.job4j.auth.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import ru.job4j.auth.AuthApplication;
 import ru.job4j.auth.domain.Person;
 import ru.job4j.auth.repository.PersonRepository;
 
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = AuthApplication.class)
 @AutoConfigureMockMvc
 class PersonControllerTest {
-    private MockMvc mockMvc;
 
     private Person item;
 
     private String itemJSON;
 
-    @Mock
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
     private PersonRepository personRepository;
 
-    @InjectMocks
-    private PersonController restController = new PersonController();
-
     @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-
-        mockMvc = MockMvcBuilders.standaloneSetup(restController)
-                .setMessageConverters(new MappingJackson2HttpMessageConverter());
-
-        Person item = myTestItemPreInitiallized;
-
+    public void setUp() throws JsonProcessingException {
+        item = new Person(1, "UserName", "pass");
         itemJSON = new ObjectMapper().writeValueAsString(item);
     }
 
@@ -64,7 +56,9 @@ class PersonControllerTest {
 
         Mockito.when(personRepository.findAll()).thenReturn(items);
 
-        mockMvc.perform(get("/person/").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+        mockMvc.perform(get("/person/")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
 
         Mockito.verify(personRepository).findAll();
     }
@@ -72,12 +66,74 @@ class PersonControllerTest {
 
     @Test
     public void testInsertSuccess() throws Exception {
-
-        Mockito.when(personRepository.save(Mockito.any(Person.class))).thenReturn(item);
-
-        mockMvc.perform(post("/person").contentType(MediaType.APPLICATION_JSON).content(itemJSON)
-                .accept(MediaType.APPLICATION_JSON)).andExpect(status().isCreated());
+        item = new Person(1, "UserName", "pass");
+        Mockito.when(personRepository.save(item)).thenReturn(item);
+        itemJSON = new ObjectMapper().writeValueAsString(item);
+        mockMvc.perform(post("/person/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(itemJSON)
+                .accept(MediaType.APPLICATION_JSON)).
+                andExpect(status().isCreated());
 
         Mockito.verify(personRepository).save(Mockito.any(Person.class));
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+        List<Person> list = List.of(
+                new Person(1, "Ivan", "123"),
+                new Person(2, "Admin", "000"),
+                new Person(3, "User1", "abc"));
+        Mockito.when(personRepository.findAll()).thenReturn(list);
+        mockMvc.perform(get("/person/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].password", is("123")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].login", is("User1")));
+    }
+
+    @Test
+    public void testFindByID() throws Exception {
+        Person person = new Person(1, "Ivan", "123");
+        Mockito.when(personRepository.findById(1)).thenReturn(Optional.of(person));
+        mockMvc.perform(get("/person/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.login", is("Ivan")));
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        Person person = new Person(1, "Ivan", "123");
+        Mockito.when(personRepository.save(person)).thenReturn(person);
+        ObjectMapper mapper = new ObjectMapper();
+        mockMvc.perform(post("/person/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(person)))
+                .andDo(print())
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        Person person = new Person(1, "User1", "123");
+        ObjectMapper mapper = new ObjectMapper();
+        Mockito.when(personRepository.save(person)).thenReturn(person);
+        mockMvc.perform(put("/person/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(person)))
+                .andDo(print())
+                .andExpect(status().isOk());
+        ArgumentCaptor<Person> captor = ArgumentCaptor.forClass(Person.class);
+        verify(personRepository).save(captor.capture());
+        assertEquals(captor.getValue().getLogin(), "User1");
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        mockMvc.perform(delete("/person/1")
+                .param("id", "1"))
+                .andExpect(status().isOk());
+
     }
 }
